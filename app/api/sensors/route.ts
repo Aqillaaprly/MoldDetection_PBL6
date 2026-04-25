ecda00a472b99cf66091606ee484eb28eb9d80e9
Aqilla Aprily <aqillaaprly@gmail.com>
2026-04-26 00:11:01 +0700

    add mri cal + automation api

--- app/api/sensors/route.ts
+++ app/api/sensors/route.ts
@@ -1,15 +1,45 @@
 import { supabase } from '@/lib/supabase'
 
 export async function GET() {
-  const { data, error } = await supabase
+  const { data: lastData } = await supabase
     .from('sensor_data')
     .select('*')
     .order('created_at', { ascending: false })
-    .limit(3)
+    .limit(1)
 
-  if (error) {
-    return Response.json({ error: error.message }, { status: 500 })
+  const last = lastData?.[0]
+
+  const smooth = (value: number, min: number, max: number, step: number) => {
+    const change = (Math.random() - 0.5) * step
+    let newValue = value + change
+
+    if (newValue < min) newValue = min
+    if (newValue > max) newValue = max
+
+    return Number(newValue.toFixed(1))
   }
+
+  // initial data
+  const newData = last
+    ? {
+        temperature: smooth(last.temperature, 20, 30, 1),
+        humidity: Math.round(smooth(last.humidity, 50, 90, 3)),
+        light: Math.round(smooth(last.light, 100, 700, 50))
+      }
+    : {
+        temperature: 25,
+        humidity: 60,
+        light: 300
+      }
+
+  // insert DB
+  await supabase.from('sensor_data').insert([newData])
+
+  const { data } = await supabase
+    .from('sensor_data')
+    .select('*')
+    .order('created_at', { ascending: false })
+    .limit(10)
 
   return Response.json(data)
 }
