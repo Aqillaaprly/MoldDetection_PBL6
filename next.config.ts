d1cc2a5a0a37a443b6cc7fe72d90da1bbeb06cd8
Febrian Arka Samudra <febrianarka10@gmail.com>
2026-05-31 20:07:44 +0700

    run

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["10.241.168.140"],
+  allowedDevOrigins: ["10.176.190.140"],
 }
 
 export default nextConfig
