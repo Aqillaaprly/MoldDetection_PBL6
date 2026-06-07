390fa9d422c6aafd8eef8502fd165ef690f06b05
sherlyazkiah <sherlyazkiah12@gmail.com>
2026-06-07 20:53:17 +0700

    run

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["10.241.168.140"],
+  allowedDevOrigins: ["10.213.180.126"],
 }
 
 export default nextConfig
